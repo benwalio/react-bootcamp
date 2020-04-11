718eb33f7b64252699b0c866a60dd7e528a11b25
ben <ben.wallace@protonmail.ch>
2020-04-11 12:51:08 -0400

    added in a defaultProps at pokedex and a map to propagate cards

--- section-5/pokedex/src/Pokedex.js
+++ section-5/pokedex/src/Pokedex.js
@@ -3,7 +3,7 @@ import Pokecard from './Pokecard';
 import './Pokedex.css';
 
 class Pokedex extends Component {
-    static defaultProps = [
+    static defaultProps = { pokemon : [
         {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
         {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
         {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
@@ -12,67 +12,15 @@ class Pokedex extends Component {
         {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
         {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
         {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
-    ]
+    ] }
     render() {
         return (
             <div className="Pokedex ui container">
                     <div className="column">
                         <div className="ui link four cards">
-                            <Pokecard
-                                name="charmander"
-                                id={4}
-                                type="fire"
-                                base_exp={62}
-                            />
-                                
-                            <Pokecard
-                                    name="squirtle"
-                                    id={7}
-                                    type="water"
-                                    base_exp={63}
-                                />
-
-                            <Pokecard
-                                name="metapod"
-                                id={11}
-                                type="bug"
-                                base_exp={72}
-                            />
-
-                            <Pokecard
-                                name="butterfree"
-                                id={12}
-                                type="flying"
-                                base_exp={178}
-                            />
-
-                            <Pokecard
-                                name="pikachu"
-                                id={25}
-                                type="electric"
-                                base_exp={112}
-                            />
-
-                            <Pokecard
-                                name="jigglypuff"
-                                id={39}
-                                type="normal"
-                                base_exp={95}
-                            />
-
-                            <Pokecard
-                                name="gengar"
-                                id={94}
-                                type="ghost"
-                                base_exp={225}
-                            />
-
-                            <Pokecard
-                                name="eevee"
-                                id={113}
-                                type="normal"
-                                base_exp={65}
-                            />
+                            {this.props.pokemon.map((p) => (
+                                <Pokecard id={p.id} name={p.name} type={p.type} base_exp={p.base_experience} />
+                            ))}
                         </div>
                     </div>
                    
