87f84251c761743bbed04685c2da01867103db1a
ben <ben.wallace@protonmail.ch>
2020-04-11 12:31:59 -0400

    finished pokedex with css and semantic ui

--- section-5/pokedex/src/Pokecard.js
+++ section-5/pokedex/src/Pokecard.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
 
+function imageClassSwitch(pokeType) {
+    switch(pokeType) {
+        case "water": 
+            return "Pokecard raised blue fluid card";
+        case "fire": 
+            return "Pokecard raised red fluid card";
+        case "bug":
+            return "Pokecard raised green fluid card";
+        case "flying":
+            return "Pokecard raised teal fluid card";
+        case "electric":
+            return "Pokecard raised yellow fluid card";
+        case "normal":
+            return "Pokecard raised grey fluid card";
+        case "ghost":
+            return "Pokecard raised purple fluid card";
+        default: 
+            return "Pokecard raised fluid card";
+    }
+}
+
 class Pokecard extends Component {
     render() {
         const {id, name, type, base_exp} = this.props;
+        const img_src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + id + ".png"
         return (
-            <div className="Pokecard">
+            <div className={imageClassSwitch(type)}>
 
+                    <div className="content">
+                        <div className="image">
+                            <img src={img_src} alt = "" />
+                        </div>
+                        <div className="center aligned header">{name}</div>
+                        <div className="meta">exp: {base_exp}</div>
+                        <div className="center aligned description"><p>type: {type}</p></div>
+                    </div>
             </div>
         );
     }
-}
+};
 
 export default Pokecard;
--- section-5/pokedex/src/Pokedex.js
+++ section-5/pokedex/src/Pokedex.js
@@ -1,18 +1,72 @@
 import React, { Component } from 'react';
 import Pokecard from './Pokecard';
+import './Pokedex.css';
 
 class Pokedex extends Component {
     render() {
         return (
-            <div className="Pokedex">
-                <div className="Pokedex-pokecard">
-                    <Pokecard
-                        name="squirtle"
-                        id={7}
-                        type="water"
-                        base_exp={63}
-                    />
-                </div>
+            <div className="Pokedex ui container">
+                    <div className="column">
+                        <div className="ui link four cards">
+                            <Pokecard
+                                name="charmander"
+                                id={4}
+                                type="fire"
+                                base_exp={62}
+                            />
+                                
+                            <Pokecard
+                                    name="squirtle"
+                                    id={7}
+                                    type="water"
+                                    base_exp={63}
+                                />
+
+                            <Pokecard
+                                name="metapod"
+                                id={11}
+                                type="bug"
+                                base_exp={72}
+                            />
+
+                            <Pokecard
+                                name="butterfree"
+                                id={12}
+                                type="flying"
+                                base_exp={178}
+                            />
+
+                            <Pokecard
+                                name="pikachu"
+                                id={25}
+                                type="electric"
+                                base_exp={112}
+                            />
+
+                            <Pokecard
+                                name="jigglypuff"
+                                id={39}
+                                type="normal"
+                                base_exp={95}
+                            />
+
+                            <Pokecard
+                                name="gengar"
+                                id={94}
+                                type="ghost"
+                                base_exp={225}
+                            />
+
+                            <Pokecard
+                                name="eevee"
+                                id={113}
+                                type="normal"
+                                base_exp={65}
+                            />
+                        </div>
+                    </div>
+                   
+                
             </div>
         );
     }
