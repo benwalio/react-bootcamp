47008a3ef08d7fbf87c8a04d44ee034c92566e58
ben <ben.wallace@protonmail.ch>
2020-04-12 20:46:10 -0400

    1586738770-autosave

--- section-9/colorblocks/src/Colorblocks.js
+++ section-9/colorblocks/src/Colorblocks.js
@@ -4,13 +4,14 @@ import './Colorblocks.css';
 
 class Colorblocks extends Component {
     static defaultProps = {
-        numColors: 18
+        numColors: 18,
+        colorArray: ["#69D2E7","#A7DBDB","#E0E4CC","#F38630","#FA6900","#588C73","#F2E394","#F2AE72","#D96459","#8C4646","#5E412F","#FCEBB6","#78C0A8","#F07818","#F0A830","#4298B5","#ADC4CC","#92B06A","#E19D29","#DD5F32","#260126","#59323C","#F2EEB3","#BFAF80","#8C6954","#F2671F","#C91B26","#9C0F5F","#60047A","#160A47"]
     }
 
     constructor(props) {
         super(props);
         this.state = {
-            colors: Array.from({ length: this.props.numColors }).fill(this.retreiveRandColor()),
+            colors: Array.from({ length: this.props.numColors }).fill(this.props.colorArray[Math.floor(Math.random() * this.props.colorArray.length)]),
             prevColor: null
         }
         this.handleClick = this.handleClick.bind(this);
@@ -19,9 +20,9 @@ class Colorblocks extends Component {
     }
 
     retreiveRandColor (prevColor) {
-        const colorArray = ["#69D2E7","#A7DBDB","#E0E4CC","#F38630","#FA6900","#588C73","#F2E394","#F2AE72","#D96459","#8C4646","#5E412F","#FCEBB6","#78C0A8","#F07818","#F0A830","#4298B5","#ADC4CC","#92B06A","#E19D29","#DD5F32","#260126","#59323C","#F2EEB3","#BFAF80","#8C6954","#F2671F","#C91B26","#9C0F5F","#60047A","#160A47"];
+        
 
-        let pickedColor = colorArray[Math.floor(Math.random() * colorArray.length)];
+        let pickedColor = this.props.colorArray[Math.floor(Math.random() * this.props.colorArray.length)];
         if (pickedColor === prevColor) {
             pickedColor = this.retreiveRandColor(prevColor);
         } else {
