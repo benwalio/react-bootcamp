c3658d208d580c7c35124c51f1b25531dac58198
ben <ben.wallace@protonmail.ch>
2020-04-14 14:38:27 -0400

    starting to parse out the alpha buttons;

---
+++ section-11/react-hangman-starter/src/AlphaButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AlphaButton extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default AlphaButton;
