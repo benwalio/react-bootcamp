4335b6761f1362e91d6a87372a99e39501458930
ben <ben.wallace@protonmail.ch>
2020-05-10 16:00:20 -0400

    working out some styles

--- colorproject/src/ColorBox.js
+++ colorproject/src/ColorBox.js
@@ -1,51 +1,144 @@
-import React, { Component } from 'react';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import styled from 'styled-jss';
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import styled from "styled-jss";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 const ColorBoxColor = styled(Grid)({
-    backgroundColor: (props) => props.color,
-    height: '14rem',
-    display: 'flex',
-    justifyContent: 'space-between',
-    '&:hover .copy-button': {
-        opacity: '1',
-        transition: '0.5s'
-    }
-})
+  backgroundColor: (props) => props.color,
+  height: "34%",
+  width: '20%',
+  display: "inline-block",
+  position: 'relative',
+  cursor: 'pointer',
+  "&:hover .copy-button": {
+    opacity: "1",
+    transition: "0.5s",
+  },
+  "& .show": {
+    opacity: 1,
+    transform: 'scale(30)',
+    zIndex: '5',
+    position: 'absolute'
+  }
+});
 
 const ColorBoxColorClickText = styled(Button)({
-    alignSelf: 'center',
-    opacity: '0',
-    // '&:hover': {
-    //     opacity: '1'
-    // }
-})
+  width: "100px",
+  position: "absolute",
+  display: "inline-block",
+  top: "50%",
+  left: "50%",
+  marginLeft: "-50px",
+  marginTop: "-15px",
+  textAlign: "center",
+  outline: "none",
+  background: "rgba(255, 255, 255, 0.3)",
+  fontSize: "1rem",
+  lineHeight: "30px",
+  textTransform: "uppercase",
+  border: "none",
+  textDecoration: "none",
+  opacity: 0
+});
 
-const ColorBoxColorNameText = styled('span')({
-    alignSelf: 'flex-end',
-    flex: '0',
-    padding: 1
-})
+const ColorBoxColorNameText = styled("span")({
+  position: "absolute",
+  width: "100%",
+  left: "0px",
+  bottom: "0px",
+  paddingBottom: ".2rem",
+  color: "black",
+  letterSpacing: "1px",
+  textTransform: "uppercase",
+  fontSize: "12px"
+});
 
 const ColorBoxMore = styled(Button)({
-    alignSelf: 'flex-end',
-    padding: '0',
-    margin: '0'
+  background: "rgba(255, 255, 255, 0.3)",
+  position: "absolute",
+  border: "none",
+  right: "0px",
+  bottom: "0px",
+  width: "60px",
+  height: "30px",
+  textAlign: "center",
+  lineHeight: "30px",
+  textTransform: "uppercase"
+});
+
+const CopyOverlayDiv = styled('div')({
+  opacity: 0,
+  backgroundColor: (props) => props.color,
+  zIndex: 0,
+  width: '100%',
+  height: '100%',
+  transition: 'transform 0.6s ease-in-out',
+  transform: 'scale(0.1)',
+})
+
+const CopyOverlayMsg = styled('div')({
+  position: 'fixed',
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  fontSize: '4rem',
+  transform: 'scale(0.1)',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  '& h1': {
+    width: '100%',
+    fontWeight: 400,
+    textShadow: [[1, 2, 'black']],
+    background: 'rgba(255, 255, 255, 0.2)'
+  }
 })
 
 class ColorBox extends Component {
-    render() {
-        const color = this.props.color;
-
-        return (
-            <ColorBoxColor item xs={3} color={color.color}>
-                <ColorBoxColorNameText>{color.name}</ColorBoxColorNameText>
-                <ColorBoxColorClickText className='copy-button'>click me</ColorBoxColorClickText>
-                <ColorBoxMore>more</ColorBoxMore>
-            </ColorBoxColor>
-        );
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       copying: false
     }
+    this.changeCopyState = this.changeCopyState.bind(this);
+  }
+
+  changeCopyState() {
+    this.setState({
+      copying: true
+    }, () => setTimeout(() => {
+      this.setState({
+        copying: false
+      })
+    }, 1500)
+    )
+  }
+  
+  render() {
+    const color = this.props.color;
+
+    return (
+      <CopyToClipboard text={color.color} onCopy={this.changeCopyState}>
+        <ColorBoxColor item xs={3} color={color.color}>
+          <CopyOverlayDiv color={color.color} className={`${this.state.copying && 'show'}`}> 
+            <CopyOverlayMsg>
+              <h1>copied</h1>
+              <p>{color.color}</p>
+            </CopyOverlayMsg>
+          </CopyOverlayDiv>
+          <ColorBoxColorNameText>{color.name}</ColorBoxColorNameText>
+          <ColorBoxColorClickText className="copy-button">
+            click me
+          </ColorBoxColorClickText>
+          <ColorBoxMore>more</ColorBoxMore>
+        </ColorBoxColor>
+      </CopyToClipboard>
+    );
+  }
 }
 
 export default ColorBox;
--- colorproject/src/Palette.js
+++ colorproject/src/Palette.js
@@ -1,97 +1,94 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // eslint-disable-next-line
-import Container from '@material-ui/core/Container';
+import Container from "@material-ui/core/Container";
 // eslint-disable-next-line
-import Grid from '@material-ui/core/Grid';
-import styled from 'styled-jss';
-import ColorBox from './ColorBox';
+import Grid from "@material-ui/core/Grid";
+import styled from "styled-jss";
+import ColorBox from "./ColorBox";
 
 const PaletteDiv = styled(Grid)({
-    display: 'flex',
-    flexDirection: 'row',
-    width: '100%',
-    height: '100%',
-    alignItems: "center",
-    justifyContent: "center",
-})
+  display: "flex",
+  flexDirection: "row",
+  width: "100%",
+  height: "100vh",
+  alignItems: "center",
+  justifyContent: "center",
+});
 
 const CommandDiv = styled(PaletteDiv)({
-    flex: '1 0',
-    backgroundColor: 'blue'
-})
+  flex: "1 0",
+  backgroundColor: "blue",
+});
 
 const ColorsGrid = styled(PaletteDiv)({
-    margin: 0,
-    padding: 0,
-    backgroundColor: 'red',
-})
+  margin: 0,
+  padding: 0,
+  backgroundColor: "red",
+  height: '90%'
+});
 
 const ColorGrid = styled(PaletteDiv)({
-    margin: 0,
-    padding: 0,
-    justifyContent: 'flex-start',
-    flexWrap: 'wrap',
-})
+  margin: 0,
+  padding: 0,
+  justifyContent: "flex-start",
+  flexWrap: "wrap",
+});
 
 const Color = styled(ColorGrid)({
-    backgroundColor: (props) => props.color,
-    height: '100px',
-})
+  backgroundColor: (props) => props.color,
+  height: "100px",
+});
 
 const TopDiv = styled(PaletteDiv)({
-    backgroundColor: 'yellow',
-    height: '4rem'
-})
+  backgroundColor: "yellow",
+  height: "4rem",
+});
 
 const BottomDiv = styled(PaletteDiv)({
-    backgroundColor: 'yellow',
-})
+  backgroundColor: "yellow",
+});
 
 class Palette extends Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            colors: this.props.colorPalette
-        };
-        this.renderColors = this.renderColors.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      colors: this.props.colorPalette,
     };
-    
-    initialState () {
-        return ({
-            colors: this.props.colorPalette
-        })
+    this.renderColors = this.renderColors.bind(this);
+  }
+
+  initialState() {
+    return {
+      colors: this.props.colorPalette,
     };
+  }
 
-    renderColors() {
-        return(
-            <ColorGrid>
-                {this.state.colors.colors.map(color =>
-                    <ColorBox name={this.state.colors.name} id={this.state.colors.id} color={color}>
-                        
-                    </ColorBox>
-                )}
-            </ColorGrid>
-        )
-    }
+  renderColors() {
+    return (
+      <ColorGrid>
+        {this.state.colors.colors.map((color) => (
+          <ColorBox
+            name={this.state.colors.name}
+            id={this.state.colors.id}
+            color={color}
+          ></ColorBox>
+        ))}
+      </ColorGrid>
+    );
+  }
 
-    render() {
-        return (
-            <PaletteDiv container>
-                <TopDiv item xs={12}>
-
-                </TopDiv>
-                {/* <CommandDiv item xs={3}>
-                    <input type="text"/>
+  render() {
+    return (
+      <PaletteDiv container>
+        <TopDiv item xs={12}></TopDiv>
+        {/* <CommandDiv item xs={3}>
+                <input type="text"/>
                 </CommandDiv> */}
-                <ColorsGrid item>
-                    {this.renderColors()}
-                </ColorsGrid>
-                <BottomDiv item xs={12}>
-
-                </BottomDiv>
-            </PaletteDiv>
-        );
-    }
+        <ColorsGrid item>{this.renderColors()}</ColorsGrid>
+        <BottomDiv item xs={12}></BottomDiv>
+      </PaletteDiv>
+    );
+  }
 }
 
 export default Palette;
